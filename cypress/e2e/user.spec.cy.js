2461ba9c635d8686f3e2acda6f06b56fdc65c3a5
mWillian-qa <mwillian.qa@gmail.com>
2025-03-30 10:27:38 -0300

    Add page object

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,62 +1,54 @@
 import userData from '../fixtures/users/user-data.json'
+import LoginPage from '../pages/loginPage'
+import DashboardPage from '../pages/dashboardPage'
+import MyInfoPage from '../pages/myInfoPage'
+import MenuPage from '../pages/menuPage'
+
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
+const myInfoPage = new MyInfoPage()
 
 describe('Orange HRM Tests', () => {
   
-  const selectorsList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
-    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']",
-    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
-    firstNameField: "[name='firstName']",
-    lastNameField: "[name='lastName']",
-    middleNameField: "[name='middleName']",
-    genericField: ".oxd-input--active",
-    dateField: "[placeholder='yyyy-dd-mm']",
-    dateButtonClose: ".--close",
-    submitButton: "[type='submit']", 
-    genericComboBox: ".oxd-select-text--active",
-    selectFirstCombobox: ".oxd-select-dropdown > :nth-child(50)",
-    selectSecondCombobox: ".oxd-select-dropdown > :nth-child(2)",
-    selectThirdCombobox: ".oxd-select-dropdown > :nth-child(6)",
-    birthDateField: "[placeholder='yyyy-dd-mm']",
+    it.only('User - Sucess', () => {
+    loginPage.accessLoginPage()
+    loginPage.loginWithAnyUser(userData.userSucess.username, userData.userSucess.password)
     
-  }
+    dashboardPage.checkDashboardPage()
+
+    menuPage.accessMyInfo()
+    
+    // myInfoPage.myInfoEdit(
+      
+    //   userData.myInfoData.firtnamefield,
+    //   userData.myInfoData.middlenamefield,
+
+    // )
 
   
-  it.only('User - Sucess', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSucess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSucess.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.dashboardGrid)
-    cy.get(selectorsList.myInfoButton).click()
-    cy.get(selectorsList.firstNameField).clear().type(userData.myInfoData.firtnamefield)
-    cy.get(selectorsList.middleNameField).clear().type(userData.myInfoData.middlenamefield)
-    cy.get(selectorsList.lastNameField).clear().type(userData.myInfoData.lastnamefield)
-    cy.get(selectorsList.genericField).eq(3).clear().type(userData.myInfoData.employeeidfield)
-    cy.get(selectorsList.genericField).eq(4).clear().type(userData.myInfoData.otheridfield)
-    cy.get(selectorsList.genericField).eq(5).clear().type(userData.myInfoData.driverslicensenumber)
-    cy.get(selectorsList.dateField).eq(0).clear().type(userData.myInfoData.datefield)
-    cy.get(selectorsList.dateButtonClose).click()
-    cy.get(selectorsList.genericComboBox).eq(0).click({force: true })
-    cy.get(selectorsList.selectFirstCombobox).click()
-    cy.get(selectorsList.genericComboBox).eq(1).click({force: true })
-    cy.get(selectorsList.selectSecondCombobox).click()
-    cy.get(selectorsList.birthDateField).eq(1).clear().type(userData.myInfoData.birthdatefield)
-    cy.get(selectorsList.dateButtonClose).click()
-
-    // espaço pra colocar a escolha do gênero
-
-    cy.get(selectorsList.genericComboBox).eq(2).click({force: true })
-    cy.get(selectorsList.selectThirdCombobox).click()
-    cy.get(selectorsList.genericField).eq(9).clear().type(userData.myInfoData.testfield)
-    cy.get(selectorsList.submitButton).eq(0).click()
-    cy.get('body').should('contain', 'Successfully Updated')
-    cy.get('.oxd-toast-close')
+    // cy.get(selectorsList.myInfoButton).click()
+    // cy.get(selectorsList.firstNameField).clear().type(userData.myInfoData.firtnamefield)
+    // cy.get(selectorsList.middleNameField).clear().type(userData.myInfoData.middlenamefield)
+    // cy.get(selectorsList.lastNameField).clear().type(userData.myInfoData.lastnamefield)
+    // cy.get(selectorsList.genericField).eq(3).clear().type(userData.myInfoData.employeeidfield)
+    // cy.get(selectorsList.genericField).eq(4).clear().type(userData.myInfoData.otheridfield)
+    // cy.get(selectorsList.genericField).eq(5).clear().type(userData.myInfoData.driverslicensenumber)
+    // cy.get(selectorsList.dateField).eq(0).clear().type(userData.myInfoData.datefield)
+    // cy.get(selectorsList.dateButtonClose).click()
+    // cy.get(selectorsList.genericComboBox).eq(0).click({force: true })
+    // cy.get(selectorsList.selectFirstCombobox).click()
+    // cy.get(selectorsList.genericComboBox).eq(1).click({force: true })
+    // cy.get(selectorsList.selectSecondCombobox).click()
+    // cy.get(selectorsList.birthDateField).eq(1).clear().type(userData.myInfoData.birthdatefield)
+    // cy.get(selectorsList.dateButtonClose).click()
+    // cy.get(selectorsList.genericComboBox).eq(2).click({force: true })
+    // cy.get(selectorsList.selectThirdCombobox).click()
+    // cy.get(selectorsList.genericField).eq(9).clear().type(userData.myInfoData.testfield)
+    // cy.get(selectorsList.submitButton).eq(0).click()
+    // cy.get('body').should('contain', 'Successfully Updated')
+    // cy.get('.oxd-toast-close')
 
 
   })
---
+++ cypress/pages/myInfoPage.js
@@ -0,0 +1,51 @@
+import userData from '../fixtures/users/user-data.json'
+
+class MyInfoPage {
+    selectorsList(){
+        const selectors = {
+            myInfoButton: "[href='/web/index.php/pim/viewMyDetails/empNumber/7']",
+            firstNameField: "[name='firstName']",
+            lastNameField: "[name='lastName']",
+            middleNameField: "[name='middleName']",
+            genericField: ".oxd-input--active",
+            dateField: "[placeholder='yyyy-dd-mm']",
+            dateButtonClose: ".--close",
+            submitButton: "[type='submit']", 
+            genericComboBox: ".oxd-select-text--active",
+            selectFirstCombobox: ".oxd-select-dropdown > :nth-child(50)",
+            selectSecondCombobox: ".oxd-select-dropdown > :nth-child(2)",
+            selectThirdCombobox: ".oxd-select-dropdown > :nth-child(6)",
+            birthDateField: "[placeholder='yyyy-dd-mm']",
+    
+        }
+
+        return selectors
+    }
+
+    myInfoEdit(){
+
+        cy.get(this.selectorsList.firstNameField).clear().type(userData.myInfoData.firtnamefield)
+        cy.get(this.selectorsList.middleNameField).clear().type(userData.myInfoData.middlenamefield)
+        cy.get(this.selectorsList.lastNameField).clear().type(userData.myInfoData.lastnamefield)
+        cy.get(this.selectorsList.genericField).eq(3).clear().type(userData.myInfoData.employeeidfield)
+        cy.get(this.selectorsList.genericField).eq(4).clear().type(userData.myInfoData.otheridfield)
+        cy.get(this.selectorsList.genericField).eq(5).clear().type(userData.myInfoData.driverslicensenumber)
+        cy.get(this.selectorsList.dateField).eq(0).clear().type(userData.myInfoData.datefield)
+        cy.get(this.selectorsList.dateButtonClose).click()
+        cy.get(this.selectorsList.genericComboBox).eq(0).click({force: true })
+        cy.get(this.selectorsList.selectFirstCombobox).click()
+        cy.get(this.selectorsList.genericComboBox).eq(1).click({force: true })
+        cy.get(this.selectorsList.selectSecondCombobox).click()
+        cy.get(this.selectorsList.birthDateField).eq(1).clear().type(userData.myInfoData.birthdatefield)
+        cy.get(this.selectorsList.dateButtonClose).click()
+        cy.get(this.selectorsList.genericComboBox).eq(2).click({force: true })
+        cy.get(this.selectorsList.selectThirdCombobox).click()
+        cy.get(this.selectorsList.genericField).eq(9).clear().type(userData.myInfoData.testfield)
+        cy.get(this.selectorsList.submitButton).eq(0).click()
+        cy.get('body').should('contain', 'Successfully Updated')
+        cy.get('.oxd-toast-close')
+    }
+
+}
+
+export default MyInfoPage
