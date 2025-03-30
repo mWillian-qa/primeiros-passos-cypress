faafcb2a8979166a8f45bef0775bc983f4584c08
mWillian-qa <mwillian.qa@gmail.com>
2025-03-30 13:34:24 -0300

    My Info page complete

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -20,36 +20,10 @@ describe('Orange HRM Tests', () => {
 
     menuPage.accessMyInfo()
     
-    // myInfoPage.myInfoEdit(
-      
-    //   userData.myInfoData.firtnamefield,
-    //   userData.myInfoData.middlenamefield,
-
-    // )
-
-  
-    // cy.get(selectorsList.myInfoButton).click()
-    // cy.get(selectorsList.firstNameField).clear().type(userData.myInfoData.firtnamefield)
-    // cy.get(selectorsList.middleNameField).clear().type(userData.myInfoData.middlenamefield)
-    // cy.get(selectorsList.lastNameField).clear().type(userData.myInfoData.lastnamefield)
-    // cy.get(selectorsList.genericField).eq(3).clear().type(userData.myInfoData.employeeidfield)
-    // cy.get(selectorsList.genericField).eq(4).clear().type(userData.myInfoData.otheridfield)
-    // cy.get(selectorsList.genericField).eq(5).clear().type(userData.myInfoData.driverslicensenumber)
-    // cy.get(selectorsList.dateField).eq(0).clear().type(userData.myInfoData.datefield)
-    // cy.get(selectorsList.dateButtonClose).click()
-    // cy.get(selectorsList.genericComboBox).eq(0).click({force: true })
-    // cy.get(selectorsList.selectFirstCombobox).click()
-    // cy.get(selectorsList.genericComboBox).eq(1).click({force: true })
-    // cy.get(selectorsList.selectSecondCombobox).click()
-    // cy.get(selectorsList.birthDateField).eq(1).clear().type(userData.myInfoData.birthdatefield)
-    // cy.get(selectorsList.dateButtonClose).click()
-    // cy.get(selectorsList.genericComboBox).eq(2).click({force: true })
-    // cy.get(selectorsList.selectThirdCombobox).click()
-    // cy.get(selectorsList.genericField).eq(9).clear().type(userData.myInfoData.testfield)
-    // cy.get(selectorsList.submitButton).eq(0).click()
-    // cy.get('body').should('contain', 'Successfully Updated')
-    // cy.get('.oxd-toast-close')
-
+    myInfoPage.fillPersonalDetails(userData.myInfoData.firstnamefield, userData.myInfoData.middlenamefield, userData.myInfoData.lastnamefield, userData.myInfoData.nicknamefield)
+    myInfoPage.fillEmployeeDetails(userData.myInfoData.employeeidfield, userData.myInfoData.otheridfield, userData.myInfoData.driverslicensenumber, userData.myInfoData.datefield, userData.myInfoData.ssnnumber, userData.myInfoData.sinnumber)
+    myInfoPage.fillStatusDetails(userData.myInfoData.birthdatefield, userData.myInfoData.testfield)
+    myInfoPage.saveForm()
 
   })
   it('Login - Fail', () => {
--- cypress/pages/myInfoPage.js
+++ cypress/pages/myInfoPage.js
@@ -1,6 +1,7 @@
 import userData from '../fixtures/users/user-data.json'
 
 class MyInfoPage {
+
     selectorsList(){
         const selectors = {
             myInfoButton: "[href='/web/index.php/pim/viewMyDetails/empNumber/7']",
@@ -21,31 +22,48 @@ class MyInfoPage {
 
         return selectors
     }
+             
+    
 
-    myInfoEdit(){
+    fillPersonalDetails(firstnamefield, middlenamefield, lastnamefield, nicknamefield) {
+       
+        cy.get(this.selectorsList().firstNameField).clear().type(userData.myInfoData.firstnamefield)
+        cy.get(this.selectorsList().middleNameField).clear().type(userData.myInfoData.middlenamefield)
+        cy.get(this.selectorsList().lastNameField).clear().type(userData.myInfoData.lastnamefield)
+        cy.get(this.selectorsList().genericField).eq(2).clear().type(userData.myInfoData.nicknamefield)
 
-        cy.get(this.selectorsList.firstNameField).clear().type(userData.myInfoData.firtnamefield)
-        cy.get(this.selectorsList.middleNameField).clear().type(userData.myInfoData.middlenamefield)
-        cy.get(this.selectorsList.lastNameField).clear().type(userData.myInfoData.lastnamefield)
-        cy.get(this.selectorsList.genericField).eq(3).clear().type(userData.myInfoData.employeeidfield)
-        cy.get(this.selectorsList.genericField).eq(4).clear().type(userData.myInfoData.otheridfield)
-        cy.get(this.selectorsList.genericField).eq(5).clear().type(userData.myInfoData.driverslicensenumber)
-        cy.get(this.selectorsList.dateField).eq(0).clear().type(userData.myInfoData.datefield)
-        cy.get(this.selectorsList.dateButtonClose).click()
-        cy.get(this.selectorsList.genericComboBox).eq(0).click({force: true })
-        cy.get(this.selectorsList.selectFirstCombobox).click()
-        cy.get(this.selectorsList.genericComboBox).eq(1).click({force: true })
-        cy.get(this.selectorsList.selectSecondCombobox).click()
-        cy.get(this.selectorsList.birthDateField).eq(1).clear().type(userData.myInfoData.birthdatefield)
-        cy.get(this.selectorsList.dateButtonClose).click()
-        cy.get(this.selectorsList.genericComboBox).eq(2).click({force: true })
-        cy.get(this.selectorsList.selectThirdCombobox).click()
-        cy.get(this.selectorsList.genericField).eq(9).clear().type(userData.myInfoData.testfield)
-        cy.get(this.selectorsList.submitButton).eq(0).click()
+
+    }
+    fillEmployeeDetails(employeeidfield, otheridfield, driverslicensenumber, datefield, ssnnumber, sinnumber) {
+
+        
+        cy.get(this.selectorsList().genericField).eq(3).clear().type(userData.myInfoData.employeeidfield)
+        cy.get(this.selectorsList().genericField).eq(4).clear().type(userData.myInfoData.otheridfield)
+        cy.get(this.selectorsList().genericField).eq(5).clear().type(userData.myInfoData.driverslicensenumber)
+        cy.get(this.selectorsList().dateField).eq(0).clear().type(userData.myInfoData.datefield)
+        cy.get(this.selectorsList().dateButtonClose).click()
+        cy.get(this.selectorsList().genericField).eq(7).clear().type(userData.myInfoData.ssnnumber)
+        cy.get(this.selectorsList().genericField).eq(8).clear().type(userData.myInfoData.sinnumber)
+    }
+    fillStatusDetails(birthdatefield, testfield) {
+
+        cy.get(this.selectorsList().genericComboBox).eq(0).click({force: true })
+        cy.get(this.selectorsList().selectFirstCombobox).click()
+        cy.get(this.selectorsList().genericComboBox).eq(1).click({force: true })
+        cy.get(this.selectorsList().selectSecondCombobox).click()
+        cy.get(this.selectorsList().birthDateField).eq(1).clear().type(userData.myInfoData.birthdatefield)
+        cy.get(this.selectorsList().dateButtonClose).click()
+        cy.get(this.selectorsList().genericComboBox).eq(2).click({force: true })
+        cy.get(this.selectorsList().selectThirdCombobox).click()
+        cy.get(this.selectorsList().genericField).eq(9).clear().type(userData.myInfoData.testfield)
+        
+    }
+
+    saveForm() {
+        cy.get(this.selectorsList().submitButton).eq(0).click()
         cy.get('body').should('contain', 'Successfully Updated')
         cy.get('.oxd-toast-close')
     }
-
 }
 
 export default MyInfoPage
